e38e96a24843adc4f3f6baa2b2a2e4d8a3a105bc
Arun Balaji R <arun.r@emeritus.org>
2024-02-20 15:13:22 +0530

    Removed max listner

--- sftp-gcs.js
+++ sftp-gcs.js
@@ -960,7 +960,7 @@ new ssh2.Server({
         logger.debug(`ERROR(client.on): ${util.inspect(err)}`);
         client.removeAllListeners();
     });
-}).setMaxListeners(50).listen(SERVER_PORT, '0.0.0.0', function () {
+}).listen(SERVER_PORT, '0.0.0.0', function () {
     logger.info("****************************************");
     logger.info("*** Google Cloud Storage SFTP Server ***");
     logger.info("****************************************");
